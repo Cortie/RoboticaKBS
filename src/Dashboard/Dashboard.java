49b786ba72e657145cc4d290996f1544fbe0e0b7
Jesper <jesper.kiers99@gmail.com>
2021-04-23 15:27:46 +0200

    added comments to Dashboard and playlist manage classes

--- src/Dashboard/Dashboard.java
+++ src/Dashboard/Dashboard.java
@@ -6,6 +6,7 @@ import java.awt.event.*;
 
 public class Dashboard extends JFrame implements ActionListener {
 
+  //definitions for labels, buttons and layouts
   JLabel temp = new JLabel("Temperatuur: ");
   JButton player = new JButton("Muziekspeler");
   JButton climate = new JButton("Klimaatbeheer");
@@ -18,16 +19,19 @@ public class Dashboard extends JFrame implements ActionListener {
   
 
   public Dashboard() {
+    //set standard data
     setTitle("Dashboard");
     setLayout(new FlowLayout(FlowLayout.CENTER));
     setSize(800, 600);
     setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+    //temperature panel
     JPanel tempPnl = new JPanel(standard);
     tempPnl.add(temp);
     temp.setFont(temp.getFont().deriveFont(32.0f));
     tempPnl.setBorder(BorderFactory.createEmptyBorder(100,0,0,0));
     
+    //panel for the middle buttons
     JPanel btnPnl = new JPanel(buttons);
     btnPnl.add(player);
     player.setPreferredSize(new Dimension(150,50));
@@ -39,11 +43,13 @@ public class Dashboard extends JFrame implements ActionListener {
     settings.setPreferredSize(new Dimension(150,50));
     settings.setFont(settings.getFont().deriveFont(16.0f));
 
+    //panel for logging out
     JPanel outPnl = new JPanel(standard);
     outPnl.add(out);
     out.setPreferredSize(new Dimension(150,50));
     out.setFont(out.getFont().deriveFont(16.0f));
 
+    //border panel serving as collection for the elements
     JPanel borderPnl = new JPanel(collection);
     collection.setVgap(100);
     borderPnl.add(tempPnl, BorderLayout.NORTH);
