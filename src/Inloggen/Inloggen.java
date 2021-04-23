5b9934bcb9ffdc3f13db1f0e74e1a6ce4648a4da
GewoonRox <74775299+GewoonRox@users.noreply.github.com>
2021-04-23 15:15:13 +0200

    update inloggen

--- src/Inloggen/Inloggen.java
+++ src/Inloggen/Inloggen.java
@@ -76,7 +76,6 @@ public class Inloggen extends JFrame implements ActionListener
 
     public static void main(String[] args)
     {
-       Inloggen inloggenscherm= new Inloggen();
-
+        Inloggen inloggenscherm= new Inloggen();
     }
 }
