943f3f0a2e9813a11fdba566848d90e438c17b07
GewoonRox <74775299+GewoonRox@users.noreply.github.com>
2021-04-23 14:14:50 +0200

    inlog schermen voor inlogggen en ww wijzigen

--- src/Inloggen/Inloggen.java
+++ src/Inloggen/Inloggen.java
@@ -1,5 +1,82 @@
 package Inloggen;
 
-public class Inloggen {
-  
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Inloggen extends JFrame implements ActionListener
+{
+    private JLabel jlTitel;
+    private JLabel jlGebruikersnaam;
+    private JLabel jlWachtwoord;
+    private JTextField jtGebruikersnaam;
+    private JTextField jtWachtwoord;
+    private JButton jbInloggen;
+    private JButton jbNieuwAccount;
+    private JButton jbVergetenWachtwoord;
+
+    public Inloggen()
+    {
+        setTitle("Klimaat systeem");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel titelPnl = new JPanel(new FlowLayout());
+        titelPnl.add(jlTitel=new JLabel("Inloggen Klimaat systeem"));
+
+        JPanel gebruikersnaamPnl = new JPanel(new FlowLayout());
+        gebruikersnaamPnl.add(jlGebruikersnaam=new JLabel("Gebruikersnaam"));
+        gebruikersnaamPnl.add(jtGebruikersnaam =new JTextField(8));
+
+        JPanel wachtwoordPnl = new JPanel(new FlowLayout());
+        wachtwoordPnl.add(jlWachtwoord=new JLabel("Wachtwoord"));
+        wachtwoordPnl.add(jtWachtwoord =new JTextField(8));
+
+        JPanel inlogknopPnl = new JPanel(new FlowLayout());
+        inlogknopPnl.add(jbInloggen = new JButton("Inloggen"));
+
+        JPanel newAccountknopPnl = new JPanel(new FlowLayout());
+        newAccountknopPnl.add(jbNieuwAccount = new JButton("Nieuw account aanmaken"));
+
+        JPanel accountknoppenPnl = new JPanel(new BorderLayout());
+        accountknoppenPnl.add(inlogknopPnl,BorderLayout.NORTH);
+        accountknoppenPnl.add(newAccountknopPnl, BorderLayout.CENTER);
+
+        JPanel wachtwoordknopPnl = new JPanel(new FlowLayout());
+        wachtwoordknopPnl.add(jbVergetenWachtwoord=new JButton("Wachtwoord vergeten"));
+
+        JPanel knoppenPnl = new JPanel(new BorderLayout());
+        knoppenPnl.add(accountknoppenPnl, BorderLayout.NORTH);
+        knoppenPnl.add(wachtwoordknopPnl,BorderLayout.CENTER);
+
+        JPanel gebruikergegevensPnl = new JPanel(new BorderLayout());
+        gebruikergegevensPnl.add(gebruikersnaamPnl,BorderLayout.NORTH);
+        gebruikergegevensPnl.add(wachtwoordPnl, BorderLayout.CENTER);
+
+        JPanel inlogPnl = new JPanel(new BorderLayout());
+        inlogPnl.add(gebruikergegevensPnl,BorderLayout.NORTH);
+        inlogPnl.add(knoppenPnl,BorderLayout.CENTER);
+
+        JPanel borderPnl = new JPanel(new BorderLayout());
+        borderPnl.add(titelPnl, BorderLayout.NORTH);
+        borderPnl.add(inlogPnl, BorderLayout.CENTER);
+
+
+
+        add(borderPnl);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+
+
+    }
+
+    public static void main(String[] args)
+    {
+       Inloggen inloggenscherm= new Inloggen();
+
+    }
 }
---
+++ src/Inloggen/WachtwoordWijzigen.java
@@ -0,0 +1,74 @@
+package Inloggen;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class WachtwoordWijzigen extends JFrame implements ActionListener
+{
+    private JLabel jlTitel;
+    private JLabel jlGebruikersnaam;
+    private JLabel jlWachtwoord;
+    private JLabel jlWachtwoordBevestigen;
+    private JTextField jtGebruikersnaam;
+    private JTextField jtWachtwoord;
+    private JTextField jtWachtwoordBevestigen;
+    private JButton jbWachtwoordWijzigen;
+
+    public WachtwoordWijzigen()
+    {
+        setTitle("Klimaat systeem");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel titelPnl = new JPanel(new FlowLayout());
+        titelPnl.add(jlTitel=new JLabel("Wachtwoord wijzigen"));
+
+        JPanel gebruikersnaamPnl = new JPanel(new FlowLayout());
+        gebruikersnaamPnl.add(jlGebruikersnaam=new JLabel("Gebruikersnaam"));
+        gebruikersnaamPnl.add(jtGebruikersnaam =new JTextField(8));
+
+        JPanel wachtwoordPnl = new JPanel(new FlowLayout());
+        wachtwoordPnl.add(jlWachtwoord = new JLabel("Wachtwoord"));
+        wachtwoordPnl.add(jtWachtwoord = new JTextField(8));
+
+        JPanel wachtwoordBevestigenPnl = new JPanel(new FlowLayout());
+        wachtwoordBevestigenPnl.add(jlWachtwoordBevestigen = new JLabel("Wachtwoord bevestigen"));
+        wachtwoordBevestigenPnl.add(jtWachtwoordBevestigen = new JTextField(8));
+
+        JPanel wachtwoordWijzigenKnopPnl = new JPanel(new FlowLayout());
+        wachtwoordWijzigenKnopPnl.add(jbWachtwoordWijzigen = new JButton("Wachtwoord wijzigen"));
+
+        JPanel wachtwoordCheckPnl = new JPanel(new BorderLayout());
+        wachtwoordCheckPnl.add(wachtwoordBevestigenPnl,BorderLayout.NORTH);
+        wachtwoordCheckPnl.add(wachtwoordWijzigenKnopPnl,BorderLayout.CENTER);
+
+        JPanel gebruikergegevensPnl =new JPanel(new BorderLayout());
+        gebruikergegevensPnl.add(gebruikersnaamPnl,BorderLayout.NORTH);
+        gebruikergegevensPnl.add(wachtwoordPnl,BorderLayout.CENTER);
+
+        JPanel gegevensPnl = new JPanel(new BorderLayout());
+        gegevensPnl.add(gebruikergegevensPnl,BorderLayout.NORTH);
+        gegevensPnl.add(wachtwoordCheckPnl,BorderLayout.CENTER);
+
+        JPanel borderPnl = new JPanel(new BorderLayout());
+        borderPnl.add(titelPnl, BorderLayout.NORTH);
+        borderPnl.add(gegevensPnl,BorderLayout.CENTER);
+
+
+
+        add(borderPnl);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        WachtwoordWijzigen wachtwoordWijzigenscherm = new WachtwoordWijzigen();
+    }
+}
