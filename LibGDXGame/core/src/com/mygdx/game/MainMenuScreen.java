5abec9dbb7955676204702399b24420cca7d471f
smellody15 <jellesmelt1507@gmail.com>
2021-04-29 12:49:48 +0200

    begin aan main menu

--- LibGDXGame/core/src/com/mygdx/game/MainMenuScreen.java
+++ LibGDXGame/core/src/com/mygdx/game/MainMenuScreen.java
@@ -5,17 +5,29 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import org.w3c.dom.Text;
 
 public class MainMenuScreen implements Screen {
     MyGdxGame game;
 
     Texture SettingsButtonInactive;
     Texture SettingsButtonActive;
+    Texture PlayButtonActive;
+    Texture PlayButtonInactive;
+    Texture ExitButtonActive;
+    Texture ExitButtonInactive;
+    int ButtonCicle;
+
     public MainMenuScreen(MyGdxGame game){
         this.game = game;
         SettingsButtonActive = new Texture("SettingsButtonActive.png");
         SettingsButtonInactive = new Texture("SettingsButtonInactive.png");
+        PlayButtonActive = new Texture("PlayButtonActive.png");
+        PlayButtonInactive = new Texture("PlayButtonInactive.png");
+        ExitButtonActive = new Texture("ExitButtonActive.png");
+        ExitButtonInactive = new Texture("ExitButtonInactive.png");
     }
+
     @Override
     public void show() {
     
@@ -27,7 +39,10 @@ public class MainMenuScreen implements Screen {
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     game.batch.begin();
     game.backgroundSprite.draw(game.batch);
-    game.batch.draw(SettingsButtonInactive, 100, 100, 300, 150);
+
+    game.batch.draw(ExitButtonInactive, 100, 100, 330, 150);
+    game.batch.draw(SettingsButtonInactive, 100, 350, 384, 85);
+    game.batch.draw(PlayButtonActive, 100, 500, 330, 150);
 
     game.batch.end();
     }
