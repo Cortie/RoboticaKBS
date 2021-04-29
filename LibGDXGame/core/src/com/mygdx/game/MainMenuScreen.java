1e4ac99aeff0e45c994c0e00e542257c35106af8
smellody15 <jellesmelt1507@gmail.com>
2021-04-29 11:10:09 +0200

    begin aan main menu

--- LibGDXGame/core/src/com/mygdx/game/MainMenuScreen.java
+++ LibGDXGame/core/src/com/mygdx/game/MainMenuScreen.java
@@ -1,5 +1,7 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 
@@ -20,7 +22,13 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void render(float delta) {
+    Gdx.gl.glClearColor(1,0, 0, 1);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    game.batch.begin();
 
+    game.batch.draw(SettingsButtonInactive, 100, 100, 300, 150);
+
+    game.batch.end();
     }
 
     @Override
