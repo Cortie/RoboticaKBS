3603721f4f743daaf00c96924996299845a90a3b
JesperKiers <jeppywho@gmail.com>
2021-05-04 23:43:28 +0200

    update python file for fetching temp from raspPi

--- src/raspberrypi/read.py
+++ src/raspberrypi/read.py
@@ -5,17 +5,6 @@ import sys
 sense = SenseHat()
 sense.clear()
 
-for x in range(0,1):
-        temp = sense.get_temperature()
-        temp = round(temp,1)
-        print("Temperature C",temp)
-        
-        humidity = sense.get_humidity()
-        humidity = round(humidity, 1)
-        print("Humidity :",humidity)
-        
-        pressure = sense.get_pressure()
-        pressure = round(pressure, 1)
-        print("Pressure:", pressure)
-        
-        time.sleep(1)
+temp = sense.get_temperature()
+temp = round(temp,1)
+print(temp)
